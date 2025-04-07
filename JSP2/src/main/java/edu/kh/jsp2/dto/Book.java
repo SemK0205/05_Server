fd4ac72d7afcab0f2da14c5ea0b131c4a3805cd0
SemK0205 <s413625@gamil.com>
2025-04-07 14:30:09 +0900

    25.04.07

---
+++ JSP2/src/main/java/edu/kh/jsp2/dto/Book.java
@@ -0,0 +1,20 @@
+package edu.kh.jsp2.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+	// 필드
+	private String title; // 제목
+	private String writer; // 작성자
+	private int price; // 가격
+	
+	// 메서드(생성자, getter/setter/toString())
+	
+	
+
+}
